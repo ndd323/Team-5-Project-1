8658252bf7b9df21a6bfd95c94643ea73c612c81
Matthew Christensen <christensenfamily05@gmail.com>
2025-03-17 10:32:06 -0500

    Springy Movement
    Implemented Input System and ship movement using a spring constraint for bouncy responsive movement

---
+++ Assets/Scripts/ShipController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+    public float moveSpeed;
+    public GameObject bulletPrefab;
+    public GameObject missilePrefab;
+    public Transform anchor;
+
+    private Vector3 anchorPos; // avoids allocating memory to a new Vec3 every frame
+
+    public ShipControls Input { get; private set; }
+    
+    private Rigidbody2D rb { get; set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var input = Game.Input.Standard;
+
+        anchor.Translate(Vector3.up * moveSpeed * input.MoveUp.ReadValue<float>() * Time.deltaTime);
+        anchor.Translate(Vector3.up * -moveSpeed * input.MoveDown.ReadValue<float>() * Time.deltaTime);
+
+        // keep us on screen
+        anchorPos.Set(anchor.position.x, Mathf.Clamp(anchor.position.y, -5f, 5f), anchor.position.z);
+        anchor.position = anchorPos;
+        
+        if (input.ShootBullet.WasPressedThisFrame())
+        {
+            var bullet = Instantiate(bulletPrefab);
+            bullet.transform.position = transform.position + (Vector3.right * 1.3f);
+        }
+
+        if (input.ShootMissile.WasPressedThisFrame())
+        {
+            var missile = Instantiate(missilePrefab);
+            missile.transform.position = transform.position + (Vector3.right * 1.3f);
+        }
+    }
+}
