4efcb0de9ae4c414c0e208b02cb0de1273f5d31d
Matthew Christensen <christensenfamily05@gmail.com>
2025-03-18 14:31:05 -0500

    Damage and Enemy (Asteroid)
    Added damage, enemy abstract class, and a simple asteroid "enemy"

---
+++ Assets/Scripts/Enemies/Asteroid.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : Enemy
+{
+    public float startSize = 1;
+
+    private float size = 1;
+    public float Size
+    {
+        get { return size; }
+        set
+        {
+            size = value;
+            ChangeSize(size);
+        }
+    }
+
+    private void ChangeSize(float newSize)
+    {
+        Health = Mathf.Ceil(newSize / 2);
+        transform.localScale = new Vector3(newSize, newSize, 1);
+    }
+
+    protected override void Start()
+    {
+        Size = startSize;
+    }
+
+    protected override void Update()
+    {
+        float moveSpeed = Game.Instance.scrollSpeed;
+
+        transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+    }
+
+}
--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class Game : MonoBehaviour
 {
+    public static Game Instance { get; private set; } // game manager singleton
     public static ShipControls Input { get; private set; }
+    public float scrollSpeed { get; private set; } // speed of our ship going right (everything else scrolling past it)
 
     // Start is called before the first frame update
     void Start()
     {
+        Instance = this;
+
         Input = new ShipControls();
         Input.Enable();
+
+        scrollSpeed = 10;
     }
 
     // Update is called once per frame
